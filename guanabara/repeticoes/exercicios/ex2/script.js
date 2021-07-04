36d330c8d6d97c0b17652c7cbabf28d659bcc362
iiizadora <oliveiras.izadora@gmail.com>
2021-07-04 17:35:45 -0300

    exercício 2 de repetições finalizados

--- guanabara/repeticoes/exercicios/ex2/script.js
+++ guanabara/repeticoes/exercicios/ex2/script.js
@@ -1,1 +1,22 @@
+const inNumero = document.querySelector('#txtn')
+const botao = document.querySelector('#btExibir')
 
+let selTab = document.querySelector('#seltab')
+
+function calcularTabuada(){
+
+  // validação
+  if(inNumero.value.length == 0){
+    alert('Por favor, digite um número!')
+  }else{
+    let numero = Number(inNumero.value)
+    selTab.innerHTML = ''
+    for(let c = 1; c <= 10;c++){
+      let item = document.createElement('option')
+      item.text=`${numero} x ${c} = ${numero*c}`
+      selTab.appendChild(item)
+    }
+  }
+}
+
+botao.addEventListener('click',calcularTabuada)
