7e2ef39a4c23110f9fd938e000dbce5c23547a7e
iiizadora <oliveiras.izadora@gmail.com>
2022-01-25 21:30:00 -0300

    function

--- origamid/funcao/script.js
+++ origamid/funcao/script.js
@@ -4,27 +4,46 @@ function isTruthy(dado) {
 }
 // Crie uma função matemática que retorne o perímetro de um quadrado
 // lembrando: perímetro é a soma dos quatro lados do quadrado
-
+function isPerimetro(lado) {
+  const total = lado * 4;
+  return total;
+}
 // Crie uma função que retorne o seu nome completo
 // ela deve possuir os parâmetros: nome e sobrenome
-
+function isName(nome, sobrenome) {
+  const myName = `Meu nome é ${nome} ${sobrenome}`;
+  return myName;
+}
 // Crie uma função que verifica se um número é par
-
+function isEven(numero) {
+  var modulo = numero % 2;
+  if (modulo === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
-
+function tipoDado(dado) {
+  return typeof dado;
+}
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
-// quando o evento 'scroll' ocorrer.
-
+// quando o evento 'click' ocorrer.
+addEventListener("click", function () {
+  console.log("Izadora");
+});
 // Corrija o erro abaixo
-/*function precisoVisitar(paisesVisitados) {
-  var totalPaises = 193;
-  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+var totalPaises = 193;
+
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam (${totalPaises - paisesVisitados}) países para visitar`;
 }
+
 function jaVisitei(paisesVisitados) {
   return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
 }
 precisoVisitar(20);
-jaVisitei(20);*/
+jaVisitei(30);
